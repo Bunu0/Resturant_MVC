4e237865e6d7e5177671f38182ac0d2d20092786
Bimalendu Bilash Prusty <131747247+Bunu0@users.noreply.github.com>
2026-04-15 22:45:42 +0530

    Create GlobalExceptionHandler.java

---
+++ exception/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception ex) {
+        return ex.getMessage();
+    }
+}
